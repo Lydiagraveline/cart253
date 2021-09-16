a603738eb0c2c7acaadbbe1966e674f5c6ad3863
lydia g <lydiag@lydias-MacBook-Air.local>
2021-09-16 19:33:35 -0400

    Update script.js

--- activities/variables experiment/js/script.js
+++ activities/variables experiment/js/script.js
@@ -11,7 +11,7 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(2000,2000);
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
