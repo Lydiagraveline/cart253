8651d9be716ca31c21a27f251e2e6039fe4e2e91
lydia g <lydiag@lydias-MacBook-Air.local>
2021-09-16 19:32:26 -0400

    played with variables successfully

---
+++ activities/variables experiment/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(2000,2000);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+  // make background fun
+  background(mouseX, mouseY, 0);
+  // square in the center
+  rectMode(CENTER);
+  rect(mouseX,mouseY,100, 100);
+}
