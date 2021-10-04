a1fc814644782d828052ac1bbbc0a8ee100e31d3
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-04 16:02:19 -0400

    A5: Started the Looking for Love activity

---
+++ activity/05-looking-for-love/js/script.js
@@ -0,0 +1,31 @@
+/**
+Activity 5: Looking for Love
+Lydia Graveline
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
