16a7f1bcdf47d793936122d08c5abf2700602b83
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-04 16:14:13 -0400

    A5: Set up the Looking for Love Activity

--- activity/05-looking-for-love/js/script.js
+++ activity/05-looking-for-love/js/script.js
@@ -6,26 +6,39 @@ Lydia Graveline
 
 "use strict";
 
+circle1 = {
+  x: undefined,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5
+};
 
-/**
-Description of preload
-*/
-function preload() {
+circle2 = {
+  x: undefined,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+};
 
-}
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCavas(windowWidth, windowHeight;
 
+    // Position circles
+    circle1.x = width / 3;
+    circle2.x = 2 * width / 3;
+
+    //start cicrles moving in random direction
+    circle1.vx = random(-circle1.speed, circle1.speed);
+    circle2.vx = random(-circle2.speed, circle2.speed);
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
+background(0);
 
 }
