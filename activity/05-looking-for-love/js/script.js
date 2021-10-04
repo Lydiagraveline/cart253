537f33df4dccaec83ed436d52ba35fd715c8661a
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-04 17:18:47 -0400

    A5: Finished Looking for Love activity!

--- activity/05-looking-for-love/js/script.js
+++ activity/05-looking-for-love/js/script.js
@@ -1,12 +1,11 @@
 /**
 Activity 5: Looking for Love
 Lydia Graveline
-
 */
 
 "use strict";
 
-circle1 = {
+let circle1 = {
   x: undefined,
   y: 250,
   size: 100,
@@ -15,7 +14,7 @@ circle1 = {
   speed: 5
 };
 
-circle2 = {
+let circle2 = {
   x: undefined,
   y: 250,
   size: 100,
@@ -24,9 +23,10 @@ circle2 = {
   speed: 5,
 };
 
+let state = `title`; //can be: title, simulation, love, sadness
 
 function setup() {
-  createCavas(windowWidth, windowHeight;
+  createCanvas (windowWidth, windowHeight);
 
     // Position circles
     circle1.x = width / 3;
@@ -37,8 +37,89 @@ function setup() {
     circle2.vx = random(-circle2.speed, circle2.speed);
 }
 
-
 function draw() {
-background(0);
+  background(0);
 
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `simulation`) {
+    simulation();
+  }
+  else if (state === `love`) {
+    love();
+  }
+  else if (state === `sadness`){
+    sadness();
+  }
 }
+
+function title() {
+  push()
+  textSize(64);
+  fill(255);
+  textAlign(CENTER,CENTER);
+  text(`LOVE?`, width/2, height/2);
+  pop()
+}
+
+function simulation() {
+  move();
+  checkOffScreen();
+  checkOverlap();
+  display();
+}
+
+function love() {
+  push()
+  textSize(64);
+  fill(200, 100, 100);
+  textAlign(CENTER,CENTER);
+  text(`LOVE!`, width/2, height/2);
+  pop()
+}
+
+function sadness() {
+  push()
+  textSize(64);
+  fill(150, 150, 255);
+  textAlign(CENTER,CENTER);
+  text(`:(`, width/2, height/2);
+  pop()
+}
+
+function move() {
+  //circle movement
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+}
+
+function checkOffScreen() {
+  //check if circles go off screen
+  if (circle1.x < 0 || circle1.x > width || circle2.x < 0 || circle2.x > width){
+    state = `sadness`
+  }
+}
+
+function checkOverlap() {
+  //check if circles overlap
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y)
+  if (d < circle1.size / 2 + circle2.size / 2) {
+    state = `love`;
+  }
+}
+
+function display() {
+  //display circles
+  ellipse (circle1.x, circle1.y, circle1.size);
+  ellipse (circle2.x, circle2.y, circle2.size);
+  }
+
+function mousePressed() {
+    if (state === `title`) {
+      state = `simulation`;
+    }
+  }
