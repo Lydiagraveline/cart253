4c5ebea424ca80d6b7369f7a0c51040843cb1c5e
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-04 18:23:30 -0400

    E3: added bouncing heart to love, actually exercise

--- exercise/love-actually/js/script.js
+++ exercise/love-actually/js/script.js
@@ -2,32 +2,65 @@
 Love, actually
 Lydia Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+love simulation game
 */
 
 "use strict";
 
+let heart = {
+  x: undefined,
+  y: undefined,
+  size: 100,
+  vx: 4,
+  vy: 5,
+  speed: 4,
+};
 
-/**
-Description of preload
-*/
+let user = {
+  x: 0,
+  y: 0,
+  size: 150
+
+}
+
+let heartImg;
+
 function preload() {
-
+  heartImg = loadImage ('assets/images/heart.png');
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+createCanvas(windowWidth, windowHeight);
+// Heart starts in center of screen
+heart.x = width/2
+heart.y = height/2
 
 }
 
+function mousePressed()
 
-/**
-Description of draw()
-*/
 function draw() {
+  background(255,192,203)
 
+  //display heart
+  imageMode(CENTER);
+  image(heartImg,heart.x,heart.y)
+  noFill();
+  ellipse(heart.x,heart.y,heart.size);
+
+  heart.x += heart.vx
+  heart.y += heart.vy
+
+  //check if heart hits edge
+  if(heart.x > width || heart.x < 0){
+    heart.vx *= -1;
+  }
+
+  if (heart.y > height || heart.y < 0) {
+    heart.vy *= -1
+
+    //display user
+    rectMode(CENTER);
+    rect(user.x, user.y, user.size, 50)
+  }
 }
