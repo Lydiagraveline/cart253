fce1dae680ac19e4a283e7682d2f32dfef79d372
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-17 13:15:43 -0400

    E4: added fish my aquarium using arrays and for loops

--- exercise/age-of-aquariums/js/script.js
+++ exercise/age-of-aquariums/js/script.js
@@ -8,26 +8,69 @@ author, and this description to match your project!
 
 "use strict";
 
+let school = []; // Create an empty array and assign it to the school variable
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(600, 600);
 
+  // Create four fish, positioned randomly, storing each one in four successive
+  for (let i = 0; i < 4; i++) {
+    school[i] = createFish(random(0, width), random(0, height));
+  }
 }
 
 
-/**
-Description of draw()
-*/
-function draw() {
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
 
+// draw()
+// Moves and displays our fish
+function draw() {
+  background(95,158,160);
+
+  for (let i = 0; i < 4; i++) {
+      moveFish(school[i]);
+      displayFish(school[i]);
+  }
+  
+}
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
 }
