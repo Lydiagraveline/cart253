1a9a741e9bfc7f3e901e5e8f58d3c676e5b3c840
lydia g <lydiag@lydias-MacBook-Air.local>
2021-10-26 16:17:56 -0400

    Ex garden: added 'Flower' class

--- exercise/juggle-garden/js/script.js
+++ exercise/juggle-garden/js/script.js
@@ -1,33 +1,16 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Garden Simulation
+Lydia Graveline
 */
 
 "use strict";
 
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
 
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
 
 }
