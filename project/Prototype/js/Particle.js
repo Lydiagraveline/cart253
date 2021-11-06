86591aa622f796a13f46fbc2a6b2a3b5be008efe
lydia g <lydiag@lydias-MacBook-Air.local>
2021-11-06 14:34:44 -0400

    Maxwell's Prototype: particles get displayed and move

--- project/Prototype/js/Particle.js
+++ project/Prototype/js/Particle.js
@@ -1,3 +1,36 @@
 class Particle {
-  
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.r = 10; // radius
+    this.vx = 5; // X velocity
+    this.vy = 2; // Y velocity
+  }
+
+move() {
+  this.x += this.vx;
+  this.y += this.vy;
+
+  // Bounces the particles off the borders on the canvas
+  if (this.x > width - this.r || this.x < this.r) {
+    this.vx = -this.vx;
+  }
+  if (this.y > height - this.r || this.y < this.r) {
+    this.vy = -this.vy;
+  }
+
+
+  // Contains the particles if the door is closed
+  if (door === `closed` && this.x > width/2 - this.r && this.x < width/2 + this.r )
+    this.vx = -this.vx;
+
+}
+
+display() {
+  push();
+  fill(255);
+  ellipse(this.x, this.y, this.r*2);
+  pop();
+}
+
 }
--- project/Prototype/js/script.js
+++ project/Prototype/js/script.js
@@ -5,17 +5,40 @@ Lydia Graveline
 
 "use strict";
 
+// The particles
+let particles = [];
+let numParticles = 10;
+
+// Program begins with the door closed
 let door = `closed`;
 
+// Set up the canvas and the particles
 function setup() {
   createCanvas(700, 500);
 
+  // Create the correct number of particles and put them in our array
+    for (let i = 0; i < numParticles; i++) {
+      let x = random(0, width);
+      let y = random(0, height);
+      let particle = new Particle(x, y);
+      particles.push(particle);
+    }
 }
 
-
+// Display and move the particles
 function draw() {
-
   displayChambers();
+
+  // Go through all the particles and move, and display them
+    for (let i = 0; i < particles.length; i++) {
+      let particle = particles[i];
+      particle.move();
+      particle.display();
+    }
+
+
+
+
 
   if (door === `closed`) {
     rectMode(CENTER);
