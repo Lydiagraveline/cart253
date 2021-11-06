efbf2db5017bf86be8eb955f490e800912294d9e
lydia g <lydiag@lydias-MacBook-Air.local>
2021-11-06 13:27:46 -0400

    Maxwell's Prototype: Created "Particle" object

--- project/Prototype/js/script.js
+++ project/Prototype/js/script.js
@@ -5,14 +5,32 @@ Lydia Graveline
 
 "use strict";
 
-let hotBalls []
-let coldBalls []
+let door = `closed`;
 
 function setup() {
-  createCanvas();
+  createCanvas(700, 500);
+
 }
 
 
 function draw() {
 
+  displayChambers();
+
+  if (door === `closed`) {
+    rectMode(CENTER);
+    fill (255);
+    rect(width/2, height/2, 10, height); // Draw thin white rectangle in center
+  }
+}
+
+// Displays the background as two chambers
+function displayChambers() {
+  noStroke();
+  //left chamber
+  background(`gray`);
+  // Right chamber
+  rectMode(CORNER);
+  fill(169);
+  rect(width/2, 0, height); // Draw gray rectangle
 }
