e71f02049a87b50a38c0bfe9bda33adc15cc7766
lydia g <lydiag@lydias-MacBook-Air.local>
2021-11-20 17:11:18 -0500

    Maxwell's Demon: Started project 2

---
+++ project/Maxwell's-Demon/js/script.js
@@ -0,0 +1,23 @@
+/**
+Maxwell's Demon
+Lydia Graveline
+
+Project 2 for CART253
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
